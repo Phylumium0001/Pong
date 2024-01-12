6b916ec671de651a5b2401e6297bdbebc7eb76a1
Phylumium <phylumium3000@gmail.com>
2024-01-12 22:21:35 +0000

    Both pads Controllable and Adjusted Screen

--- pong.py
+++ pong.py
@@ -24,6 +24,10 @@ ball_rect = ball_surf.get_rect(center=(300,200))
 direction_x = choice([2,-2])
 direction_y = choice([2,-2])
 
+
+# Pausing Game
+pause = False
+
 while True:
     for event in pygame.event.get():
 
@@ -33,17 +37,38 @@ while True:
 
         # When a key is pressed
         if event.type == pygame.KEYDOWN:
+            # Continue
+            if event.key == pygame.K_SPACE:
+                if ball_rect.center == (300,200):
+                    direction_x = choice([2,-2])
+                    direction_y = choice([2,-2])
+
             # Move up
             if event.key == pygame.K_w:
-                if pad1_rect.topleft[1] >= 0:
+                # Prevent the pad from leaving the screen
+                if pad1_rect.topleft[1] >= 35:
                     pad1_rect.y -= 20
                 
 
             # Move down
             if event.key == pygame.K_s:
-                pad1_rect.y += 20
+                if pad1_rect.bottomleft[1] <= 395:
+                    pad1_rect.y += 20
 
-    screen.blit(sky_obj, (0,0))
+            # pad2
+            # Move up
+            if event.key == pygame.K_UP:
+                # Prevent the pad from leaving the screen
+                if pad2_rect.topleft[1] >= 35:
+                    pad2_rect.y -= 20
+                
+
+            # Move down
+            if event.key == pygame.K_DOWN:
+                if pad2_rect.bottomleft[1] <= 395:
+                    pad2_rect.y += 20
+
+    screen.blit(sky_obj, (0,25))
 
     pygame.draw.rect(screen,"Red", pad1_rect)
     pygame.draw.rect(screen,"Blue",pad2_rect)
@@ -52,12 +77,11 @@ while True:
     # direction = ('left','right','bottom','right')
 
     # Ball Motion
-    #Up
-    if ball_rect.midtop[1] <= 2:
-        print(ball_rect.midtop)
+    #Upward Collision
+    if ball_rect.midtop[1] <= 27:
         direction_y = 2
 
-    # Down
+    # Down Collision
     if ball_rect.midbottom[1] >= 400:
         direction_y = -2
 
@@ -69,15 +93,30 @@ while True:
     if ball_rect.midright[0] >= 600:
         direction_x = -2 
 
+    # Collisions
+
+    # Pad1 collision
+    if pad1_rect.collidepoint(ball_rect.left,ball_rect.y):
+        print(ball_rect.left)
+        if direction_x == 2:direction_x = -2
+        else: direction_x = 2
+
+    # Pad2 collision
+    if pad2_rect.collidepoint(ball_rect.right,ball_rect.y):
+        print(ball_rect.right)
+        if direction_x == 2:direction_x = -2
+        else: direction_x = 2
+
+    if ball_rect.left < 25 or ball_rect.right > 575:
+        ball_rect.center = (300,200)
+        pygame.draw.ellipse(screen,"Yellow",ball_rect)
+        direction_y = 0
+        direction_x = 0
+
     ball_rect.y += direction_y
     ball_rect.x += direction_x
-
-    # print(ball_rect.center)
-
-    # Collisions
-    if pad1_rect.collidepoint(ball_rect.midleft):
-        direction_x,direction_y = direction_y,direction_x
-        print("Collision")
+    # print(pad1_rect.topright)
+        
     pygame.draw.ellipse(screen,"Yellow",ball_rect)
 
     pygame.display.update()
